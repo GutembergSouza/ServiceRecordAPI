d3aab0efb55a8cafd9e3c3f65554c6fa2517070e
GutembergSiqueira96 <gutembergsiqueira96@gmail.com>
2021-08-12 14:25:39 -0300

    first commit

---
+++ src/main/java/dio/gu/servicerecordapi/ServiceRecordApiApplication.java
@@ -0,0 +1,13 @@
+package dio.gu.servicerecordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRecordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRecordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/gu/servicerecordapi/ServiceRecordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.gu.servicerecordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRecordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
